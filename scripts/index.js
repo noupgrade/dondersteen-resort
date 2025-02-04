2ca0384dcb566cd20f39f4d73d443c62eb802c27
Jorge Aleman gonzalez <jorge.alemangonzalez@gmail.com>
2025-02-04 20:51:38 +0100

    Write version working

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,13 @@ const { setConfig } = require('./config')
 const designateAsStaff = require('./admins/designateAsStaff')
 const listStaffUsers = require('./admins/listStaffUsers')
 const runMigration = require('./platform/migrations/runMigration')
-
+const writeVersion = require('./platform/writeVersion')
 program.option('--prod', 'Run in production mode', false)
 
 program.addCommand(designateAsStaff)
 program.addCommand(listStaffUsers)
 program.addCommand(runMigration)
-
+program.addCommand(writeVersion)
 program.hook('preAction', command => {
     console.log(`Running in ${command.opts().prod ? 'PROD' : 'LOCAL'} env`)
     setConfig('prod', command.opts().prod)
--- scripts/platform/writeVersion.js
+++ scripts/platform/writeVersion.js
@@ -1,25 +1,33 @@
 const admin = require('firebase-admin')
-const {execSync} = require('child_process')
-const {getFirestoreInstance} = require("./firebase");
+const { execSync } = require('child_process')
+const { getFirestoreInstance } = require('./firebase')
+const { Command } = require('commander')
 
-const isLocal = process.argv.includes('--local')
+const command = new Command('write-version')
 
-if (isLocal) {
-    firestore.settings({
-        host: "localhost:5003",
-        ssl: false
-    })
-}
-
+command
+    .option(
+        '--is-breaking [value]',
+        'Indicates if the new version has a breaking change',
+        value => {
+            if (typeof value === 'string') {
+                return value.toLowerCase() === 'true'
+            }
+            return value
+        },
+        false,
+    )
+    .action(isBreaking => writeNewVersion(isBreaking))
 
 // Get commit hash from Git
 const commitHash = execSync('git rev-parse --short HEAD').toString().trim()
 
-// Check for breaking change flag from environment variable
-const isBreaking = process.env.IS_BREAKING_CHANGE === 'true'
+const isBreakingEnv = process.env.IS_BREAKING_CHANGE === 'true'
 
 // Write the new version to Firestore
-const writeNewVersion = async () => {
+const writeNewVersion = async options => {
+    const isBreaking = options.isBreaking || isBreakingEnv
+
     try {
         const newVersion = {
             commitHash,
@@ -27,11 +35,11 @@ const writeNewVersion = async () => {
             isBreaking,
         }
         await getFirestoreInstance().collection('app_versions').add(newVersion)
-        console.log('Version written to Firestore:', {newVersion})
+        console.log('Version written to Firestore:', { newVersion })
     } catch (error) {
         console.error('Error writing version to Firestore:', error)
         process.exit(1)
     }
 }
 
-writeNewVersion()
+module.exports = command
